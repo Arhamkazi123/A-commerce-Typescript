505d9172ef7fc622f4aa0e8d43faeaca6f5070c9
Arham kazi <kaziarham2@gmail.com>
2024-08-03 19:03:49 +0530

    finishing login

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -1,18 +1,33 @@
 import { useState } from "react"
 import Navbar from "../components/Navbar"
 import "./Login.css"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
 
+interface Formdata{
+    email:string;
+    password:string;
+}
 
 const Login:React.FC = () => {
 
-    const [form,setform]=useState{
-
+    const nav=useNavigate();
+    const [fd,setfd]=useState<Formdata>({
         email:"",
         password:""
-        
-    }
+    })
+    
     const handlelogin=async(e:React.FormEvent)=>{
         e.preventDefault();
+        try{
+            const res=await axios.post("http://localhost:8000/api/auth/login",fd);
+            alert("Logged in succssfully");
+            nav("/");
+
+        }catch(error){
+            alert("Not able to login");
+            console.log(error);
+        }
     }
   return (
     <>
@@ -24,8 +39,22 @@ const Login:React.FC = () => {
 
     <div className="loginmainbody">
         <form onSubmit={handlelogin}>
-            <input type="text" placeholder="Enter registered email" value={}/>
-            <input type="text" placeholder="Enter password" />
+            <input 
+            type="text" 
+            placeholder="Enter registered email" 
+            name="email"
+            value={fd.email} 
+            autoComplete="off"
+            onChange={(e)=>setfd({ ...fd, [e.target.name]: e.target.value })
+            
+            }/>
+            <input type="password" 
+            placeholder="Enter password" 
+            autoComplete="off"
+            name="password"
+            value={fd.password} 
+            onChange={(e)=>setfd({ ...fd, [e.target.name]: e.target.value })}/>
+
             <button type="submit">Login</button>
         </form>
     </div>
